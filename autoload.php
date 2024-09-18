885082a4810f113ba7d6000829a84ae73d9bfa6c
RichardJesse <jesse.richard@strathmore.edu>
2024-09-18 23:23:27 +0300

    setting up the repository

---
+++ autoload.php
@@ -0,0 +1,46 @@
+<?php
+
+if (!function_exists('autoloader')) {
+    /**
+     * Autoloads all the php classes from the app directory
+     * @param $class
+     * @return void
+     */
+    function autoloader($class)
+    {
+        $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+        $baseDir = __DIR__ . '/app';
+
+        $filePath = recursiveFileSearch($baseDir, $classPath);
+
+
+        if ($filePath !== null && file_exists($filePath)) {
+            require_once $filePath;
+        }
+    }
+
+    /**
+     * recursively searches a given directory and returns the correct file path
+     * 
+     * @param $directory
+     * @param $classPath
+     * @return
+     */
+    function recursiveFileSearch($directory, $classPath)
+    {
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS)
+        );
+
+        foreach ($files as $file) {
+            if (strpos($file->getRealPath(), $classPath) !== false) {
+                return $file->getRealPath();
+            }
+        }
+
+        return null;
+    }
+
+    spl_autoload_register('autoloader');
+
+}
