cafd48f83a20ce693005e6ae99ba7acdb5dd1891
RichardJesse <jesse.richard@strathmore.edu>
2024-09-20 20:24:06 +0300

    added a contant url to ensure uniformity

--- app/Constants/Constants.php
+++ app/Constants/Constants.php
@@ -6,3 +6,4 @@ define("HOSTUSER", "root");
 define("HOSTPASS", "");
 define("DBNAME", "crud_task");  
 define("MAIL_FROM_ADDRESS", 'jesse.richard@strathmore.edu');
+define("APP_URL", 'http://localhost/boostrapSignUp/app');
