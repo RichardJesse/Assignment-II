d94c6cd3ed6233a07c5254b92c4ec35256ae6e5a
RichardJesse <jesse.richard@strathmore.edu>
2024-09-20 20:17:27 +0300

    working on the email verification

--- app/Constants/Constants.php
+++ app/Constants/Constants.php
@@ -5,3 +5,4 @@ define("DBPORT", "3306");
 define("HOSTUSER", "root");
 define("HOSTPASS", "");
 define("DBNAME", "crud_task");  
+define("MAIL_FROM_ADDRESS", 'jesse.richard@strathmore.edu');
---
+++ app/Traits/CanSendEmail.php
@@ -0,0 +1,108 @@
+<?php
+
+// include_once __DIR__ . '/../../autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+include_once __DIR__ . '/../Constants/Constants.php';
+
+trait CanSendEmail
+{
+    protected $mail;
+
+    
+    /**
+     * instanciates the php mailer instance
+     *
+     * @return $this
+     */
+    public function mail()
+    {
+
+        $this->mail = new PHPMailer(true);
+        return $this;
+
+    }
+
+    /**
+     * set from who the email is from
+     *
+     * @param $email
+     * @return $this
+     */
+    public function from($email = MAIL_FROM_ADDRESS)
+    {
+
+        $this->mail->setFrom($email);
+        return $this;
+    }
+
+    /**
+     * set who the email is going to
+     *
+     * @param $email
+     * @return $this
+     */
+    public function to($email)
+    {
+        $this->mail->addAddress($email);
+        return $this;
+    }
+
+     /**
+     * Add a subject to the email
+     *
+     * @param $subject
+     * @return $this
+     */
+    public function subject($subject)
+    {
+        $this->mail->Subject = $subject;
+        return $this;
+    }
+
+     /**
+     * makes the html in the file the body of the email
+     *
+     * @param $path
+     * @return $this
+     */
+    public function html($content)
+    {
+        $this->mail->msgHTML($content);
+        return $this;
+    }
+
+     /**
+     * add an attachment to the email
+     *
+     * @param $path
+     * @return $this
+     */
+    public function attach($path)
+    {
+        $this->mail->addAttachment($path);
+        return $this;
+    }
+
+    /**
+     * sends the email
+     *
+     * @return string|true
+     */
+    public function send()
+    {
+        try {
+            if (!$this->mail->send()) {
+
+                throw new Exception('Mailer Error: ' . $this->mail->ErrorInfo);
+                return 'sent';
+            }
+            return true;
+        } catch (Exception $e) {
+            return 'Error: ' . $e->getMessage();
+        }
+    }
+
+}
