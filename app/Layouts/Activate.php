771815909a32cfafbacbf0783c7bb29ecb6b70b8
RichardJesse <jesse.richard@strathmore.edu>
2024-09-23 22:08:00 +0300

    final design for informing user activation message sent

--- app/Layouts/Activate.php
+++ app/Layouts/Activate.php
@@ -10,11 +10,15 @@ class Activate
         <div class="d-flex justify-content-center align-items-center vh-100">
             <div class="card shadow rounded-4" style="width: 70vw; height: 70vh;">
 
-                <div class="card-body d-flex justify-content-center align-items-center">
+                <div class="card-body  d-flex flex-column justify-content-center align-items-center">
                     <div>
                         <iframe src="https://lottie.host/embed/7f895a1f-e19c-4fce-b7db-e8cc43f2c05e/7mP4wDQVah.json"></iframe>
                     </div>
-                    A verification email has been sent to <span class="fw-bolder"><?php echo $email ?></span>
+                    <div class="display-6 text-center" style="font-family: SUSE;">
+
+                       A verification email has been sent to <span class="fw-bolder text-info"><?php echo $email ?></span>
+
+                    </div>
                 </div>
             </div>
         </div>
