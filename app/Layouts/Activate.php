64d2a58199f6ba6e7fbbfe5f87e2c2d095dc70c9
RichardJesse <jesse.richard@strathmore.edu>
2024-09-23 21:08:42 +0300

    activation page created

--- app/Layouts/Activate.php
+++ app/Layouts/Activate.php
@@ -9,12 +9,16 @@ class Activate
 ?>
         <div class="d-flex justify-content-center align-items-center vh-100">
             <div class="card shadow rounded-4" style="width: 70vw; height: 70vh;">
+
                 <div class="card-body d-flex justify-content-center align-items-center">
-                    A verification email has been sent to <?php echo $email ?>
+                    <div>
+                        <iframe src="https://lottie.host/embed/7f895a1f-e19c-4fce-b7db-e8cc43f2c05e/7mP4wDQVah.json"></iframe>
+                    </div>
+                    A verification email has been sent to <span class="fw-bolder"><?php echo $email ?></span>
                 </div>
             </div>
         </div>
 <?php
-return ob_get_clean();
+        return ob_get_clean();
     }
 }
