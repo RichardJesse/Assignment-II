5e98085f40b876c64a74a99551ccb465286f2e7b
RichardJesse <jesse.richard@strathmore.edu>
2024-09-25 14:03:56 +0300

    added the right path to the favicon

--- app/Layouts/Tags.php
+++ app/Layouts/Tags.php
@@ -20,7 +20,7 @@ class Tags
             <meta charset="UTF-8">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link rel="icon" type="image/x-icon" href="../Animation - 1726328937110.gif">
+            <link rel="icon" type="image/x-icon" href="app\Animation - 1726328937110.gif">
             <link href="https://fonts.googleapis.com/css2?family=SUSE:wght@100..800&display=swap" rel="stylesheet">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
             <title><?php echo $title ?></title>
