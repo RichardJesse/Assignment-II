8fcd4521647be852764ef1e1a587b705368c85e9
RichardJesse <jesse.richard@strathmore.edu>
2024-09-25 16:06:21 +0300

    got that table name automatically for the name of the entity

--- app/Entities/AbstractEntities.php
+++ app/Entities/AbstractEntities.php
@@ -17,31 +17,46 @@ class AbstractEntities
     }
 
     /**
-     * gets the pularized name of the class in lowercase
-     * 
-     * 
-     * 
-     * 
+     * Get the class "basename" of the given object / class.
+     *
+     * @param  string|object  $class
+     * @return string
      */
-   public static function pluralizeClassname(string $classname) {
-       
-        $parts = explode('\\', $classname);
-        $class = end($parts);
-    
-       
-        // $classLower = strtolower($class);
-    
-        
-        // $pluralized = $classLower . 's';
-    
-        return $classname;
+    public static function classBasename($class)
+    {
+        $class = is_object($class) ? get_class($class) : $class;
+
+        return basename(str_replace('\\', '/', $class));
     }
 
+
+    /**
+     * Returns the potential table name for the entity
+     * 
+     * 
+     * @param $class
+     * 
+     */
+    public static function getTableName($class)
+    {
+        $classLower = strtolower($class);
+        $pluralized = $classLower . 's';
+
+        return $pluralized;
+    }
+
+    /**
+     * 
+     * Used to access the Query class for the 
+     * Query builder package
+     * 
+     */
     public static function query()
     {
         $query = new \HemiFrame\Lib\SQLBuilder\Query();
         $entity = new \ReflectionClass(get_called_class());
-        $entity->getShortName();
-        return $query->from(explode('\\', $entity));
+        $className = $entity->getShortName();
+
+        return $query->from(self::getTableName($className));
     }
 }
