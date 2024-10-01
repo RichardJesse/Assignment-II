dda664067cc41965137c36250106fdb73d9d4956
RichardJesse <jesse.richard@strathmore.edu>
2024-10-01 12:38:38 +0300

    'feat: Implement new layouts for Main Page, Modal and User Profile

---
+++ app/Layouts/UserProfile.php
@@ -0,0 +1,36 @@
+<?php
+
+class UserProfile
+{
+    
+    public function content($currentUser)
+    {
+        ob_clean();
+?>
+        <form id="signup-form" class="w-100 align-items-center" method="post" action="signUp_logic.php">
+            <div class="form-floating mb-3 w-75 mx-auto">
+                <input name="username" class="form-control focus-ring focus-ring-info" type="text" placeholder="Username" aria-label="Username" required>
+                <label for="floatingInput">Username</label>     
+            </div>
+
+            <div class="form-floating mb-3 w-75 mx-auto">
+                <input name="email" class="form-control  focus-ring focus-ring-info" type="email" placeholder="Email" aria-label="Email" required>
+                <label for="floatingInput">Email</label>
+                
+            </div>
+
+            <div class="form-floating mb-3 w-75 mx-auto">
+                <input name="password" class="form-control  focus-ring focus-ring-info" type="password" placeholder="Password" aria-label="Password" required>
+                <label for="floatingInput">Password</label>
+                
+            </div>
+
+            <div class="mb-3 w-75 mx-auto">
+                <button type="submit" class="btn btn-outline-info w-100">Edit</button>
+            </div>
+        </form>
+
+<?php
+        return ob_get_clean();
+    }
+}
